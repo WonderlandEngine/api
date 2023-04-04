4f77540d0c1b10f782aa34ec64eed63b766314af
rafern <rafern@protonmail.com>
2023-04-04 12:36:07 +0100

    Remove MappedType, not used anymore

--- src/wonderland.ts
+++ src/wonderland.ts
@@ -9,17 +9,6 @@ import {Emitter} from './utils/event.js';
 import {ComponentProperty} from './property.js';
 
 /**
- * Maps a type to another type.
- *
- * @template {any} T - The input type.
- * @template {any} R - The type relation map. A type which has indices of pair, where each pair has the type [T, V], where T is the input type, and V is the output type.
- */
-export type MappedType<T, R> = {
-    [k in keyof R]:
-        R[k] extends [T, infer P] ? P : never
-}[keyof R];
-
-/**
  * A type alias for any TypedArray constructor, except big-int arrays.
  */
 export type TypedArrayCtor = Int8ArrayConstructor | Uint8ArrayConstructor | Uint8ClampedArrayConstructor | Int16ArrayConstructor | Uint16ArrayConstructor | Int32ArrayConstructor | Uint32ArrayConstructor |  Float32ArrayConstructor | Float64ArrayConstructor;
