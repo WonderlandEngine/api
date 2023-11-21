a8fea6db091fb8dfedb30eb8e5d44fbe5a38d50f
Pipo <eliaducceschi@hotmail.it>
2023-11-21 12:38:44 +0100

    Use const instead of let

--- src/wonderland.ts
+++ src/wonderland.ts
@@ -5683,9 +5683,9 @@ export class RayHit {
         if (!out) out = Array.from({ length: this.hitCount }, () => new Float32Array(3));
 
         const wasm = this._engine.wasm;
-        let alignedPtr = this._ptr / 4; /* Align F32 */
+        const alignedPtr = this._ptr / 4; /* Align F32 */
         for (let i = 0; i < this.hitCount; ++i) {
-            let locationPtr = alignedPtr + 3 * i;
+            const locationPtr = alignedPtr + 3 * i;
             out[i][0] = wasm.HEAPF32[locationPtr];
             out[i][1] = wasm.HEAPF32[locationPtr + 1];
             out[i][2] = wasm.HEAPF32[locationPtr + 2];
@@ -5717,9 +5717,9 @@ export class RayHit {
         if (!out) out = Array.from({ length: this.hitCount }, () => new Float32Array(3));
 
         const wasm = this._engine.wasm;
-        let alignedPtr = (this._ptr + 48) / 4; /* Align F32 */
+        const alignedPtr = (this._ptr + 48) / 4; /* Align F32 */
         for (let i = 0; i < this.hitCount; ++i) {
-            let normalPtr = alignedPtr + 3 * i;
+            const normalPtr = alignedPtr + 3 * i;
             out[i][0] = wasm.HEAPF32[normalPtr];
             out[i][1] = wasm.HEAPF32[normalPtr + 1];
             out[i][2] = wasm.HEAPF32[normalPtr + 2];
@@ -5752,9 +5752,9 @@ export class RayHit {
         if (!out) out = new Float32Array(this.hitCount);
 
         const wasm = this._engine.wasm;
-        let alignedPtr = (this._ptr + 48 * 2) / 4; /* Align F32 */
+        const alignedPtr = (this._ptr + 48 * 2) / 4; /* Align F32 */
         for (let i = 0; i < this.hitCount; ++i) {
-            let distancePtr = alignedPtr + i;
+            const distancePtr = alignedPtr + i;
             out[i] = wasm.HEAPF32[distancePtr];
         }
         return out;
@@ -5785,9 +5785,9 @@ export class RayHit {
         if (!out) out = new Array(this.hitCount);
 
         const HEAPU16 = this._engine.wasm.HEAPU16;
-        let alignedPtr = (this._ptr + (48 * 2 + 16)) >> 1;
+        const alignedPtr = (this._ptr + (48 * 2 + 16)) >> 1;
         for (let i = 0; i < this.hitCount; ++i) {
-            let objectPtr = alignedPtr + i;
+            const objectPtr = alignedPtr + i;
             out[i] = this._engine.wrapObject(HEAPU16[objectPtr + i]);
         }
         return out;
